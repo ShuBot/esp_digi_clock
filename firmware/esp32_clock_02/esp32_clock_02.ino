049b3d9d79382f0703b13ccca7a77d6a96faec44
Shubham Mohapatra <01shubham10@gmail.com>
2025-10-29 08:28:25 +0530

    Updates time update freq on display. code cleanup

--- firmware/esp32_clock_02/esp32_clock_02.ino
+++ firmware/esp32_clock_02/esp32_clock_02.ino
@@ -4,7 +4,6 @@
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
-
 #include <WiFi.h>
 #include "time.h"
 
@@ -13,74 +12,49 @@
 #define PRINT(...) Serial.print(__VA_ARGS__)
 #define PRINTS(...) Serial.println(__VA_ARGS__)
 #endif
-// Defining size, and output pins
-// GPIO18	CLK
 
-// GPIO23	DIN
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES   4   // Total number of LED modules connected in parallel.
 #define CS_PIN        5   // GPIO5	CS
-// #define DM_INTENSITY  0   // Dot-Matrix LED Intensity Range: 0-15
-int DM_INTENSITY = 0;
 
 MD_Parola Display = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
-const char* ssid     = "ShuBot11";
-const char* password = "mochubabu99";
+const char* ssid      = "ShuBot11";
+const char* password  = "mochubabu99";
 
-const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 19800;      // GMT+5:30 Offset time for IST
-const int   daylightOffset_sec = 0;
-unsigned long startTime = 0;
+const char* ntpServer           = "pool.ntp.org";
+const long  gmtOffset_sec       = 19800;      // GMT+5:30 Offset time for IST
+const int   daylightOffset_sec  = 0;
+unsigned long startTime         = 0;
 
-uint8_t scrollSpeed = 75;    // default frame delay value - 25
-textEffect_t scrollEffect = PA_SCROLL_DOWN;
-textPosition_t scrollAlign = PA_CENTER;
-uint16_t scrollPause = 60000; // in milliseconds
+uint8_t DM_INTENSITY        = 0;     // Dot-Matrix LED Intensity Range: 0-15
+uint8_t scrollSpeed         = 75;    // default frame delay value - 25
+uint16_t scrollPause        = 1000; // in milliseconds
 
-// Global message buffers shared by Serial and Scrolling functions
+// Global message buffers shared by Scrolling functions
 #define	BUF_SIZE	10
 char curMessage[BUF_SIZE] = { "xxx" };
 char waitMessage[BUF_SIZE] = { "-----" };
 char newMessage[BUF_SIZE] = { "Hello!" };
-bool newMessageAvailable = true;
 
-void readSerial(void)
-{
-  static char *cp = newMessage;
-
-  while (Serial.available())
-  {
-    *cp = (char)Serial.read();
-    if ((*cp == '\n') || (cp - newMessage >= BUF_SIZE-2)) // end of message character or full buffer
-    {
-      *cp = '\0'; // end the string
-      // restart the index for next filling spree and flag we have a message waiting
-      cp = newMessage;
-      newMessageAvailable = true;
-    }
-    else  // move char pointer to next position
-      cp++;
-  }
-}
-
+// Setup, sets display config parameters, gets time from NTP over wifi.
 void setup(){
   Display.begin();
   Display.setIntensity(DM_INTENSITY);
-  Display.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
+  Display.displayText(curMessage, PA_CENTER, scrollSpeed, scrollPause, PA_PRINT, PA_NO_EFFECT);
   // Display.displayClear();
 
   if(SERIAL_DEBUG) {Serial.begin(115200);}
   // Connect to Wi-Fi
-  PRINT("Connecting to ");
-  PRINTS(ssid);
+  if(SERIAL_DEBUG) {PRINT("Connecting to ");}
+  if(SERIAL_DEBUG) {PRINTS(ssid);}
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    PRINT(".");
+    if(SERIAL_DEBUG) {PRINT(".");}
   }
-  PRINTS("");
-  PRINTS("WiFi connected.");
+  if(SERIAL_DEBUG) {PRINTS("");}
+  if(SERIAL_DEBUG) {PRINTS("WiFi connected.");}
   
   // Init and get the time
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
@@ -92,26 +66,25 @@ void setup(){
   // WiFi.mode(WIFI_OFF);
 }
 
+// Main loop()
 void loop(){
 
-  if (Display.displayAnimate())
-  {
+  if (Display.displayAnimate()) {
       Display.displayReset();
       printLocalTime();
       delay(10);
-    
   }
-  // readSerial();
 }
 
+// Updates "curMessage" buffer with the right time data.
 void printLocalTime(){
   struct tm timeinfo;
   if(!getLocalTime(&timeinfo)){
-    PRINTS("Failed to obtain time");
+    if(SERIAL_DEBUG) {PRINTS("Failed to obtain time");}
     strcpy(curMessage, waitMessage);
   } else {
-    PRINTS("Local time obtained successfully");
-    PRINTS(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+    if(SERIAL_DEBUG) {PRINTS("Local time obtained successfully");}
+    if(SERIAL_DEBUG) {PRINTS(&timeinfo, "%A, %B %d %Y %H:%M:%S");}
     
     // Change DM_INTENSITY as per hour of day.
     if(timeinfo.tm_hour >= 6 && timeinfo.tm_hour <= 17) {
